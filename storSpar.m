4d7f7e6ba71592ccebe78b940c1a83c8f76ee8ef
Madhusa <madhusak@gmail.com>
2014-10-23 12:39:29 +1000

    updated Sparse code

--- storSpar.m
+++ storSpar.m
@@ -1,4 +1,4 @@
-function [sparMatrix,cholSpar] = storSpar(coMatrix)
+function [sparMatrix,cholSpar,solSpar] = storSpar(coMatrix,b)
 %% Cholesky with Sparse Storage and AMD node rodering
 % 1. Generates AMD node reordered coMatrix
 % 2. Generates Sparse Storage of reordered coMatrix
@@ -6,6 +6,8 @@ function [sparMatrix,cholSpar] = storSpar(coMatrix)
 % 4. Computes solution from banded cholesky
 %% Parameters
 n = 33;
+solSpar = b;
+coMatrix = triu(coMatrix);
 %% AMD Node reordering
 
 permAMD = symamd(coMatrix);
@@ -17,10 +19,35 @@ spy(amdReOrder)
 sparMatrix = sparse(amdReOrder);
 
 cholSpar = sparMatrix;
+tic;
 for i = 1:n
     if i ~= 1
-        cholSpar(i,i:n) = (cholSpar(i,i:n) - cholSpar(1:i-1,i)'*cholSpar(1:i-1,i:n));
+        cholSpar(i,i:n) = cholSpar(i,i:n) - cholSpar(1:i-1,i)'*cholSpar(1:i-1,i:n);
     end
     cholSpar(i,i:n) = cholSpar(i,i:n)/sqrt(cholSpar(i,i));
 end
+toc;
 
+% cholSpar = full(cholSpar);
+
+
+%% Forward Substituion Gx = b
+for i = 1:n
+    if i ~= 1
+        solSpar(i) = solSpar(i) - dot(cholSpar(1:i-1,i),solSpar(1:i-1));
+    end
+    
+    solSpar(i) = solSpar(i)/cholSpar(i,i);
+end
+
+
+% 
+% %% Back Subsitution G'z=x
+% 
+for i = n:-1:1
+    if i ~= n
+        solSpar(i) = solSpar(i) - dot(cholSpar(i,i+1:n),solSpar(i+1:n));
+    end
+    
+    solSpar(i) = solSpar(i)/cholSpar(i,i);
+end
